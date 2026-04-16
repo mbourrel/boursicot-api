3c207dcf59efc5d2f22941ae5a5b1bdbe64b5012
Mateo <b00821404@essec.edu>
2026-04-16 10:53:44 +0200

    modif pas comprise

--- database.py
+++ database.py
@@ -13,6 +13,16 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
     SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# --- LA CORRECTION EST ICI ---
+# On prépare les arguments de connexion
+connect_args = {}
+# Si l'URL contient "render.com", on force le SSL car Render l'exige
+if "render.com" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"sslmode": "require"}
+
+# On injecte les connect_args dans l'engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+# -----------------------------
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
